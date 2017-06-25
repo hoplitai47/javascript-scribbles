5d8e4ab8edb1f9ca6e426a43b65765cd59bca54d
PG <hoplitai47@gamil.com>
2017-06-25 16:05:01 +0200

    experimenting with styles

--- core.js
+++ core.js
@@ -1,34 +1,21 @@
 
-/*
-var table = document.createElement('table');
-for (var i = 1; i < 4; i++){
-    var tr = document.createElement('tr');   
-
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-
-    var text1 = document.createTextNode('Text1');
-    var text2 = document.createTextNode('Text2');
-
-    td1.appendChild(text1);
-    td2.appendChild(text2);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-
-    table.appendChild(tr);
-}
-document.body.appendChild(table);
-*/
-
 
 var tileMap = new Map();
+var executionDisableReason = ['ed_last_look', 'ed_zero_volume',
+                        'ed_bad_tick', 'ed_bad_price', 'ed_wide_spread', 'ed_manual'];
 
 
 
 function createNewTile (tileId) {
 	var newTile = document.createElement('div');
+    var randReason = executionDisableReason[Math.floor(Math.random() * executionDisableReason.length)];
+    
     $(newTile).html(tileId)
-                .attr('id', tileId);
+        .addClass('tile')
+        .addClass(randReason)
+        .attr('id', tileId);
+
+
 
     return newTile;
 }
@@ -58,16 +45,7 @@ function handleInput() {
 
 
 
-//$(document).ready();
+$(document).ready(console.log('Working!'));
 
 
 
-
-
-
-
-
-
-
-
-console.log('Working!')
