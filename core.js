dadbaf2d87322e88f95e3a19b21791a86e0b0f7d
PG <hoplitai47@gamil.com>
2017-06-25 20:56:38 +0200

    add first bit of classes

--- core.js
+++ core.js
@@ -1,35 +1,90 @@
 
 
 var tileMap = new Map();
-var executionDisableReason = ['ed_last_look', 'ed_zero_volume',
-                        'ed_bad_tick', 'ed_bad_price', 'ed_wide_spread', 'ed_manual'];
+var executionDisableReason = ['ed_last_look',
+                        'ed_zero_volume',
+                        'ed_bad_tick',
+                        'ed_bad_price',
+                        'ed_wide_spread',
+                        'ed_manual'];
 
+//  CLASS BEGIN
+//  DisableReasonMapper
+class DisableReasonMapper {
+    constructor() {
+        this.mapping = new Map()
+            .set("EXECUTION_DISABLE_LAST_LOOK", "ed_last_look")
+            .set("EXECUTION_DISABLE_ZERO_VOLUME", "ed_zero_volume")
+            .set("EXECUTION_DISABLE_BAD_TICK", "ed_bad_tick")
+            .set("EXECUTION_DISABLE_BAD_PRICE", "ed_bad_price")
+            .set("EXECUTION_DISABLE_WIDE_SPREAD", "ed_wide_spread")
+            .set("EXECUTION_DISABLE_MANUAL", "ed_manual");
 
+    }
 
-function createNewTile (tileId) {
-	var newTile = document.createElement('div');
-    var randReason = executionDisableReason[Math.floor(Math.random() * executionDisableReason.length)];
-    
-    $(newTile).html(tileId)
-        .addClass('tile')
-        .addClass(randReason)
-        .attr('id', tileId);
-
-
-
-    return newTile;
+    toStyleName() {
+        return this.mapping.has()
+    }
 }
+//  CLASS END
+
+
+//  CLASS BEGIN
+//  TileFactory
+class TileFactory {
+    constructor() {
+        this.currentDivID = 1; 
+    }
+
+    createTile(tileName) {
+        var ret = new Tile(tileName, this.currentDivID);
+        this.currentDivID++;
+        return ret;
+    }
+
+    createTile(tileName, tileId) {
+        var ret = new Tile(tileName, tileId);
+        return ret;
+    }
+}
+//  CLASS END
+
+
+//  CLASS BEGIN 
+//  Tile 
+function Tile(name, divId) {
+    this.name = name;
+    this.divId = divId;
+
+    this.asElement = document.createElement('div');
+    $(this.asElement).html(this.name)
+        .addClass('tile')
+        .attr('id', this.divId);
+
+}
+Tile.prototype.getAsElement = function() {
+    return this.asElement; 
+}
+Tile.prototype.getDivId = function() {
+    return this.divId;
+}
+
+//  CLASS END
+
 
 function handleTile(tileId) {
     if(tileMap.has(tileId)) {
+        var tile = tileMap.get(tileId);
         tileMap.delete(tileId);
 
-        $('#tile_area').find('#' + tileId).remove();
+        $('#tile_area').find('#' + tile.getDivId()).remove();
     } else {
-        var newTile = createNewTile(tileId);
+        tileFactory.createTile
+
+        var newTile = tileFactory.createTile(tileId, tileId);
         tileMap.set(tileId, newTile)
 
-        $('#tile_area').append(newTile);
+        $('#tile_area').append(newTile.getAsElement());
     }
     
 }
@@ -44,6 +99,7 @@ function handleInput() {
 }
 
 
+var tileFactory = new TileFactory();
 
 $(document).ready(console.log('Working!'));
 
